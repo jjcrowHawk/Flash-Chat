8960de0184f545ec9540b3a85dba806e65c9c733
jjcrow <hawkcrowxx@gmail.com>
2022-04-13 22:19:15 -0500

    Setting up CocoaPods dependency Manager

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = "⚡️FlashChat"
+        
+        /*
+        // ANIMATION WITH FOR LOOP AND TIME SCHEDULER
+         
+        let titleText = "⚡️FlashChat"
+         
+         
+        var charIndex = 0.0
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            
+            charIndex += 1
+        }
+         
+        */
     }
     
 
